21bc0de8581d2071a7e08c41a9580999dd152ca7
Riley Moore <rilemoore@gmail.com>
2019-10-23 21:00:56 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,28 @@
 class BankAccount
 
+  attr_accessor :balance, :status,
+  attr_reader :name, 
+
+  def initialize(name)
+    @name = name
+    @balance = 1000
+    @status = "open"
+  end
+
+  def deposit(deposit_amount)
+    @balance += deposit_amount
+  end
+
+  def display_balance
+    puts @balance
+    @balance
+  end
+  
+  def valid?
+    if(@status == "open" && @balance > 0)
+      true
+    end
+    false
+  end
+  
 end
