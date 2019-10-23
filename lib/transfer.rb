e431c588f2b83b85a0dd3555450b545dd5d2fa67
Riley Moore <rilemoore@gmail.com>
2019-10-23 21:06:33 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,18 @@
 class Transfer
   
+  attr_accessor :sender, :receiver, :status, :transfer_amount
+  
+  def initialize(sender, receiver, transfer_amount)
+    
+    @sender = sender
+    @receiver = receiver
+    @transfer_amount = transfer_amount
+    @status = "pending"
+    
+  end
+  
+  def valid?
+    
+  end
+  
 end
